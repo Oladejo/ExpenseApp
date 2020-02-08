799aea23502fe7352e961b3e45b1998ef1358d27
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-02-08 23:20:02 +0100

    remove unused code

--- InitsProject/DTO/ExpenseDTO.cs
+++ InitsProject/DTO/ExpenseDTO.cs
@@ -1,27 +1,13 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System;
 
 namespace InitsProject.DTO
 {
     public class ExpenseDTO
     {
-        [JsonProperty("id")]
         public int Id { get; set; }
-
-        [JsonProperty("value")]
-        [Required(ErrorMessage = "Required Expense value")]
         public decimal Value { get; set; }
-
-        [JsonProperty("reason")]
-        [Required(ErrorMessage = "Required Reason for the expense")]
         public string Reason { get; set; }
-
-        [JsonProperty("DateofExpense")]
-        [Required(ErrorMessage = "Require date of the expense")]
         public DateTime Date { get; set; }
-
-        [JsonProperty("vat")]
         public decimal VAT { get; set; }
     }
 }
